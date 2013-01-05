24d4a8da1f6ed0903c6ba4e4d95f30f8517d4c97
Joe Zack <me@joezack.com>
2013-01-05 00:52:47 -0500

    Fixing capitilization

--- colormine/Properties/AssemblyInfo.cs
+++ colormine/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("colormine")]
+[assembly: AssemblyTitle("ColorMine")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("colormine")]
+[assembly: AssemblyProduct("ColorMine")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

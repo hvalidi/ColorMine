d50d807ee9bed5301b96e9e0888298529e7668e4
Joe Zack <me@joezack.com>
2013-01-06 11:30:31 -0500

    Getting rid of color space equality for now, adds unnecessary complexity

--- colormine/ColorSpaces/ColorSpace.cs
+++ colormine/ColorSpaces/ColorSpace.cs
@@ -21,34 +21,6 @@ namespace ColorMine.ColorSpaces
         }
         private const int DataSize = 3;
         private readonly double[] _data = new double[DataSize];
-        public override string ToString()
-        {
-            return _data.ToString();
-        }
-
-        public override int GetHashCode()
-        {
-            // TODO This doesn't make any sense, should behave like a value type
-            return _data.ToString().GetHashCode();
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (null == obj) return false;
-
-            var colorSpaceObj = obj as ColorSpace;
-            if (null == colorSpaceObj) return false;
-
-            // TODO Should convert to appropriate color space first!
-
-            for (var i = 0; i < _data.Length; i++)
-            {
-                if (!this[i].IsCloseTo(colorSpaceObj[i])) return false;
-            }
-
-            return true;
-        }
-
         internal double this[int index]
         {
             get

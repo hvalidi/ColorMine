0fb0f3aee36631687a193ba7a05ea930ba4985b2
Joe Zack <me@joezack.com>
2013-01-06 13:09:31 -0500

    Finishing tests for Rgb, getting them passing

--- colormine/ColorSpaces/Rgb.cs
+++ colormine/ColorSpaces/Rgb.cs
@@ -1,12 +1,13 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 
 namespace ColorMine.ColorSpaces
 {
     public class Rgb : ColorSpace
     {
-        public double R { get { return this[0]; } set { this[0] = value; } }
-        public double G { get { return this[1]; } set { this[1] = value; } }
-        public double B { get { return this[2]; } set { this[2] = value; } }
+        public double R { get { return this[0]; } set { ValidateRange(value); this[0] = value; } }
+        public double G { get { return this[1]; } set { ValidateRange(value); this[1] = value; } }
+        public double B { get { return this[2]; } set { ValidateRange(value); this[2] = value; } }
 
         public override void Initialize(Color color)
         {
@@ -19,6 +20,16 @@ namespace ColorMine.ColorSpaces
         {
             // TODO Not right...
             return Color.FromArgb(255, (int)R, (int)G, (int)B);
+        }
+
+        private const double Min = 0;
+        private const double Max = 255;
+        private void ValidateRange(double n)
+        {
+            if (n < Min || n > Max)
+            {
+                throw new ArgumentOutOfRangeException(n + " must be between " + Min + " and " + Max);
+            }
         }
     }
 }

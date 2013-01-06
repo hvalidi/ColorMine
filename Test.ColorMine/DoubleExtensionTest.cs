71f99cbffbfc6afb984bc5523980dbba5c81fb9f
Joe Zack <me@joezack.com>
2013-01-06 12:19:42 -0500

    Adding first tests, setting up friendly assemblies

---
+++ Test.ColorMine/DoubleExtensionTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ColorMine.Utility;
+
+namespace Test.ColorMine
+{
+    [TestClass]
+    public class DoubleExtensionTest
+    {
+        [TestClass]
+        public class BasicallyEqualTo
+        {
+            [TestMethod]
+            public void ReturnsTrueForCloseNumbers()
+            {
+                Assert.IsTrue(.3333.BasicallyEqualTo(1.0/3,.001));
+            }
+
+            [TestMethod]
+            public void ReturnsFalseForFarNumbers()
+            {
+                Assert.IsFalse(.9.BasicallyEqualTo(.1,.001));
+            }
+        }
+    }
+}
--- Test.ColorMine/Properties/AssemblyInfo.cs
+++ Test.ColorMine/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ColorMine")]
+[assembly: AssemblyTitle("Test.ColorMine")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ColorMine")]
+[assembly: AssemblyProduct("Test.ColorMine")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5b29e3b7-5264-4ad2-b05f-db8165f179e7")]
+[assembly: Guid("8240b1b4-7061-4f80-886f-d3e475476982")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,5 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: InternalsVisibleTo("Test.ColorMine")]
